e4af05b923da764277143d19adb90c92323ecef9
yeji <shfks1212@naver.com>
2022-02-09 22:34:33 +0900

    [section7] 제네릭 제약 조건

--- section7/dist/app.js
+++ section7/dist/app.js
@@ -2,5 +2,24 @@
 function merge(objA, ojbB) {
     return Object.assign(objA, ojbB);
 }
-const mergeObj = merge({ name: 'max' }, { age: 30 });
+// const mergeObj = merge({ name: 'max' }, 30); // => Error
+const mergeObj = merge({ name: 'max' }, { age: 30 }); // => Error
 console.log(mergeObj.name);
+function countAndDescribe(element) {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = `Got 1 element.`;
+    }
+    else if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements`;
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe('hi there '));
+console.log(countAndDescribe(['a', 'b']));
+// console.log(countAndDescribe(123)); //=> Error
+function extractAndConvert(obj, key) {
+    return obj[key];
+}
+// extractAndConvert({}, 'name') // => Error
+extractAndConvert({ name: 'max' }, 'name'); // => Error
--- section7/src/app.ts
+++ section7/src/app.ts
@@ -1,9 +1,37 @@
-function merge<T, U>(objA: T, ojbB: U) {
+function merge<T extends object, U extends object>(objA: T, ojbB: U) {
   return Object.assign(objA, ojbB);
 }
 
-const mergeObj = merge<{ name: string }, { age: number }>(
-  { name: 'max' },
-  { age: 30 }
-);
+// const mergeObj = merge({ name: 'max' }, 30); // => Error
+const mergeObj = merge({ name: 'max' }, { age: 30 }); // => Error
+
 console.log(mergeObj.name);
+
+// 문자열이든 배열이든 상관없이, length 속성을 지니는지만 신경쓰면 된다.
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = `Got 1 element.`;
+  } else if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('hi there '));
+console.log(countAndDescribe(['a', 'b']));
+// console.log(countAndDescribe(123)); //=> Error
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return obj[key];
+}
+
+// extractAndConvert({}, 'name') // => Error
+extractAndConvert({ name: 'max' }, 'name'); // => Error
